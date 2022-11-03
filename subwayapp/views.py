a4645278ba613fd4a4bff9eea420774e79bf7c44
lgb2002 <cccavin12345@naver.com>
2022-11-03 16:04:54 +0900

    fifth commit

--- subwayapp/views.py
+++ subwayapp/views.py
@@ -4,7 +4,8 @@ from .models import Post
 import requests
 import json
 
-secret_key = "5a6e7753626361763837624c6f7856"
+#secret_key = "5a6e7753626361763837624c6f7856"
+secret_key = "sample"
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -28,18 +29,35 @@ def matching(request):
     requestData = requests.get(requestUrl)
     jsonData = requestData.json()
 
-    stationName = jsonData.get("statnNm")
-    if(stationName is None):
+    jsonDataList = jsonData["realtimePositionList"]
+
+    stationName = ""
+
+    for data in jsonDataList:
+        if trainNumber == data["trainNo"]:
+            stationName = data["statnNm"]
+
+    if(stationName == ""):
         return render(request, 'subwayapp/post_list.html', {'arriveTimeSec': "결과가 없습니다"})
 
     requestUrl = "http://swopenAPI.seoul.go.kr/api/subway/" + \
-                  secret_key + "/" + fileType + "/realtimePosition/" + \
+                  secret_key + "/" + fileType + "/realtimeStationArrival/" + \
                   "/" + idx_start + "/" + idx_last + "/" + stationName
 
     requestData = requests.get(requestUrl)
     jsonData = requestData.json()
 
-    arriveTimeSec = jsonData.get("barvlDt")
+    jsonDataList = jsonData["realtimeArrivalList"]
+
+    arriveTimeSec = ""
+
+    for data in jsonDataList:
+        if data["btrainNo"] == trainNumber:
+            arriveTimeSec = data["barvlDt"]
+
+    if(arriveTimeSec == ""):
+        return render(request, 'subwayapp/post_list.html', {'arriveTimeSec': "결과가 없습니다2"})
+
     return render(request, 'subwayapp/post_list.html', {'arriveTimeSec': arriveTimeSec})
 
 
