d4a3b9463761bcdee7fc185cce3fbe9f8b79c893
lgb2002 <cccavin12345@naver.com>
2022-11-03 15:11:55 +0900

    third  commit

--- subwayapp/views.py
+++ subwayapp/views.py
@@ -1,4 +1,43 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+import requests
+import json
+
+secret_key = "5a6e7753626361763837624c6f7856"
 
 def post_list(request):
-    return render(request, 'subwayapp/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'subwayapp/post_list.html', {'posts': posts})
+
+def matching(request):
+    subwayLineNumber = request.POST['subwayLineNumber']
+    trainNumber = request.POST['trainNumber']
+    getOffStationName = request.POST['getOffStationName']
+    userNickname = request.POST['userNickname']
+
+    fileType = "json"
+
+    idx_start = "0"
+    idx_last = "5"
+
+    requestUrl = "http://swopenAPI.seoul.go.kr/api/subway/" + \
+                  secret_key + "/" + fileType + "/realtimePosition/" + \
+                  "/" + idx_start + "/" + idx_last + "/" + subwayLineNumber
+
+    requestData = requests.get(requestUrl)
+    jsonData = requestData.json()
+
+    stationName = jsonData.get("statnNm")
+
+    requestUrl = "http://swopenAPI.seoul.go.kr/api/subway/" + \
+                  secret_key + "/" + fileType + "/realtimePosition/" + \
+                  "/" + idx_start + "/" + idx_last + "/" + stationName
+
+    requestData = requests.get(requestUrl)
+    jsonData = requestData.json()
+
+    arriveTimeSec = jsonData.get("barvlDt")
+    return render(request, 'subwayapp/post_list.html', {'arriveTimeSec': arriveTimeSec})
+
+
